23278a0c8f5b3e7d64e977aa554aee87f56894a7
Zuckonit <lsin30@foxmail.com>
2013-11-20 10:06:19 +0800

    use ajax to load image

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,19 @@
-var refreshId = setInterval(function() {
-    var bid = document.getElementById('banner');
-    var i = Math.floor(Math.random() * 4);
-    bid.src = "img/banner/totoro/" + i + ".jpg";
-    i += 1;
-    i %= 4;
-}, 6000);
+var i = 0;
+var max_i = 3;
+
+function displayImage()
+{
+    var img = document.createElement("img");
+    img.setAttribute("width", "100%");
+    img.setAttribute("height", "100%");
+
+    img.onload = function () {
+        var div = document.getElementById("banner");
+        while (div.childNodes.length > 0)
+            div.removeChild(div.childNodes[0]);
+        div.appendChild(img);
+        (i == max_i) ? i = 0: i++;
+        window.setTimeout("displayImage();", 2000);
+    };
+    img.src = "img/banner/totoro/" + i + ".jpg";
+}
