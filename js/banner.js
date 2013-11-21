6ad036f39ec633e21fe113a6d3fd21ac63ef7fc8
Zuckonit <lsin30@foxmail.com>
2013-11-21 10:55:40 +0800

    rollback

--- js/banner.js
+++ js/banner.js
@@ -1,19 +1,7 @@
-var i = 0;
-var max_i = 3;
-
-function displayImage()
-{
-    var img = document.createElement("img");
-    img.setAttribute("width", "100%");
-    img.setAttribute("height", "100%");
-
-    img.onload = function () {
-        var div = document.getElementById("banner");
-        while (div.childNodes.length > 0)
-            div.removeChild(div.childNodes[0]);
-        div.appendChild(img);
-        (i == max_i) ? i = 0: i++;
-        window.setTimeout("displayImage();", 5000);
-    };
-    img.src = "img/banner/totoro/" + i + ".jpg";
-}
+var refreshId = setInterval(function() {
+    var bid = document.getElementById('banner');
+    var i = Math.floor(Math.random() * 4);
+    bid.src = "img/banner/totoro/" + i + ".jpg";
+    i += 1;
+    i %= 4;
+}, 6000);
